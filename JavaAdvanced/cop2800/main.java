baa16d30985b6faa10b8f3efee1725730a7ab02d
bobojohanna <91637865+bobojohanna@users.noreply.github.com>
2022-08-24 23:19:19 -0400

    Initial commit for advanced Java class

---
+++ JavaAdvanced/cop2800/main.java
@@ -0,0 +1,9 @@
+package JavaAdvanced.cop2800;
+
+public class main {
+    public static void main(String[] args){
+
+		test t = new test("Johanna", 20);
+        System.out.println(t.toString());
+	}
+}
---
+++ JavaAdvanced/cop2800/test.java
@@ -0,0 +1,20 @@
+package JavaAdvanced.cop2800;
+
+public class test {
+
+	public String name;
+	public int age;
+
+	
+
+	public test(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String toString() {
+
+		return " Pre-game interview: My name is " + this.name + " and I am " + this.age + " years old ";
+	}
+
+}
