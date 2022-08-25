9bee160bd8aa8c6be576e369de8f48649c41a899
bobojohanna <91637865+bobojohanna@users.noreply.github.com>
2022-08-24 23:31:20 -0400

    committing directory

---
+++ cop2805C/Main.java
@@ -0,0 +1,11 @@
+package cop2805C;
+
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println("\n\nRunning from cop2805C package");
+
+		Scratch t = new Scratch("Johanna", 20);
+        System.out.println(t.toString() + "\n\n");
+	}
+}
---
+++ cop2805C/Scratch.java
@@ -0,0 +1,20 @@
+package cop2805C;
+
+public class Scratch {
+
+	public String name;
+	public int age;
+
+	
+
+	public Scratch(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String toString() {
+
+		return " Pre-game interview: My name is " + this.name + " and I am " + this.age + " years old ";
+	}
+
+}
