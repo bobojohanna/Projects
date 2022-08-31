01f3299bc4d404187a2d51f177361934f6c8c1a1
bobojohanna <91637865+bobojohanna@users.noreply.github.com>
2022-08-30 22:03:27 -0400

    Update Array.java

--- cop2805C/Array.java
+++ cop2805C/Array.java
@@ -1,30 +1,33 @@
 package cop2805C;
-import java.util.Arrays;
-public class Array<E> {
-    private final Object[] obj_array;  
-    public final int length;
-   
-    public Array(int length)    {
-        obj_array = new Object [length];
-        this.length = length;
-    } 
-    E get(int i) {
-        @SuppressWarnings("unchecked")
-        final E e = (E)obj_array[i];
-        return e;
+
+public class Array {
+
+	public static int findIndex(int array[], int value)
+    {
+  
+      
+        if (array == null) {
+            return -1;
+        }
+  
+       
+        int len = array.length;
+        int i = 0;
+  
+       
+        while (i < len) {
+  
+            
+            //return the index
+            if (array[i] == value) {
+                return i;
+            }
+            else {
+                i = i + 1;
+            }
+        }
+        return -1;
     }
-   
-    void set(int i, E e) {
-        obj_array[i] = e;
-    }
-    @Override
-    public String toString() {
-        return Arrays.toString(obj_array);
-    }
-    public Object[] getObj_array() {
-        return obj_array;
-    }
-    public int getLength() {
-        return length;
-    }
+
+
 }
