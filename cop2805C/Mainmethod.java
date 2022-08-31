6c81eb37ccad30e9948973729db6074542558011
bobojohanna <91637865+bobojohanna@users.noreply.github.com>
2022-08-30 22:02:40 -0400

    Update Mainmethod.java

--- cop2805C/Mainmethod.java
+++ cop2805C/Mainmethod.java
@@ -1,21 +1,20 @@
 package cop2805C;
 
+
+
 public class Mainmethod {
 
-
+        
+       
         public static void main(String[] args){
-            final int length = 6;
-           
-            Array<Integer>int_Array = new Array<Integer>(length);
-            System.out.print("Generic Array:" + " ");
-            for (int i = 0; i < length; i++)
-                int_Array.set(i, i * 1);
-            System.out.println(int_Array);
-            Array<String>str_Array = new Array<String>(length);
-            System.out.print("Generic Array:" + " ");
-            for (int i = 0; i < length; i++)
-                str_Array.set(i, String.valueOf((char)(i + 97)));
-            System.out.println(str_Array);
+            int[] array = {4, 9, 7, 3, 2, 8};  
+            int  value = 2;
+            int indexOfValue = (array.length);
+            System.out.println("Index of " + value + " is " + indexOfValue);
+
         }
+
+        
+
+      
     }
-    
