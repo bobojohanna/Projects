917efcde6a6087912d4bdceb25fff90af050090a
bobojohanna <91637865+bobojohanna@users.noreply.github.com>
2022-08-31 23:13:00 -0400

    Update Array.java

--- cop2805C/Array.java
+++ cop2805C/Array.java
@@ -4,32 +4,32 @@ package cop2805C;
 
 public class Array {
 
-	
-	private int index;
-    public int findIndex(int[] array, int value)
-    {
-
-
-        if (array == null) {
+private int index;
+        public int findIndex(int[] array, int value, float[] floatArray, float fValue, String[] strArray, String StrValue)
+        {
+    
+    
+            if (array == null) {
+                return -1;
+            }
+      
+           
+            int len = array.length;
+            int i = 0;
+      
+           
+            while (i < len) {
+      
+                
+                //return the index
+                if (array[i] == value) {
+                    return i;
+                }
+                else {
+                    i = i + 1;
+                }
+            }
             return -1;
         }
-  
-       
-        int len = array.length;
-        int i = 0;
-  
-       
-        while (i < len) {
-  
-            
-            //return the index
-            if (array[i] == value) {
-                return i;
-            }
-            else {
-                i = i + 1;
-            }
-        }
-        return -1;
     }
-}
+    }
