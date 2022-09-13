2a3d9de52836dbbaeb7e95ad6603e716b2f1cd3a
bobojohanna <91637865+bobojohanna@users.noreply.github.com>
2022-09-13 12:23:50 -0400

    Update Collect.java

--- cop2805C/Collect.java
+++ cop2805C/Collect.java
@@ -7,53 +7,65 @@ import java.util.List;
 
 public class Collect {
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
 
-        /**
-         * You're missing the comments he's asking for. In this case,
-         * you can say: Instatiating ArrayList object.
-         */
-        ArrayList<String> cities = new ArrayList<String>(); //get arraylist
+		// Instatiating ArrayList object
+		ArrayList<String> cities = new ArrayList<String>(); // get arraylist
 
-        // Adding cities to ArrayList object
-        cities.add("Melbourne");
-        cities.add("Orlando");
-        cities.add("Miami");
-        cities.add("Tampa");
-        cities.add("Naples");
-        cities.add("Lakeland");
+		// Adding cities to ArrayList object
+		cities.add("Melbourne");
+		cities.add("Orlando");
+		cities.add("Miami");
+		cities.add("Tampa");
+		cities.add("Naples");
+		cities.add("Lakeland");
 
-        // Printing header message
-        System.out.println("Original List:");
+		// Printing header message
+		System.out.println("Original List:");
 
-        // Iterating over the ArrayList object
-        for (String ct : cities)
-            System.out.println(ct);
+		// Iterating over the ArrayList object
+		for (String ct : cities)
+			System.out.println(ct);
 
-        Collections.shuffle(cities); // randomizing the list
-        System.out.println("Randomized List: " + cities);  // print the randomized list
-        Collections.sort(cities); //alphabetizing the list
-        System.out.println("Alphabetized List: " + cities); // print the alphabatized list
-        System.out.println("Index Found In: " + cities.indexOf("Miami")); // print where city index is found
-        System.out.println("Index Found In: " + cities.indexOf("Jacksonville")); //search city that's not in list
-        System.out.println("Array: ");
-        String ct[] = cities.toArray(new String[cities.size()]); //converting original list to array using toArray() method
-        for (String str : ct) { //iterate string array
-            System.out.println(str); //print string array
+		// Randomizing the list
+		Collections.shuffle(cities);
+		// Print the randomized list
+		System.out.println("Randomized List: " + cities);
+		// Alphabetizing the list
+		Collections.sort(cities);
+		// Print the alphabatized list
+		System.out.println("Alphabetized List: " + cities);
+		// Print where city index is found
+		System.out.println("Index Found In: " + cities.indexOf("Miami"));
+		// Search city that's not in list
+		System.out.println("Index Found In: " + cities.indexOf("Jacksonville"));
+		System.out.println("Array: ");
 
-            // Point 6 should be done outside the loop and have its own loop
-            List<String> c = new ArrayList<String>(Arrays.asList(ct)); //convert array to array list 
-            System.out.println("List: " + c);    //print array list
-        }
+		// Arraylist to array conversion
+		String ctt[] = new String[cities.size()];
+		for (int j = 0; j < cities.size(); j++) {
+			ctt[j] = cities.get(j);
+		}
+		// Displaying array 
+		for (String k : cities) {
+			// Print string array
+			System.out.println(k);
+            
+		}
+		// Convert array to array list
+		List<String> c = new ArrayList<String>(Arrays.asList(ctt));
 
-    }
-    /* Collection is the interface for classifying objects
-   into one entity. Collections is a utility class that 
-   implements tasks on Collection. An interface is a sort
-   of reference and it is a collection of abstracted 
-   operations. A class utilizes an interface and 
-   acquires the interface's abstract methods. 
-   Sources: upgrad.com , www.tutorialspoint.com */
+		// Print array list
+		System.out.println("List: " + c); // print array list
+	}
+
+	/*
+	 * Collection is the interface for classifying objects into one entity.
+	 * Collections is a utility class that implements tasks on Collection. An
+	 * interface is a sort of reference and it is a collection of abstracted
+	 * operations. A class utilizes an interface and acquires the interface's
+	 * abstract methods. Sources: upgrad.com , tutorialspoint.com
+	 */
 }
 
 
